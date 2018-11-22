3e0cf8be8c3a8d4739a4c22b465b1a830e0a2ed1
sivakumar <srramas@amazon.com>
2018-11-22 16:59:02 +1100

    changes

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def createData():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=8080)
-    #app.run(debug=True, host='0.0.0.0', port=80)
+    #app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=80)
