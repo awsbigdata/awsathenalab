bbc7a84b8a093321dc16ea9013210e430de2de4e
sivakumar <sivankumar86@gmail.com>
2018-11-23 10:31:29 +1100

    install as root

--- com/awssupport/athenalab/mockdata/exercise1.py
+++ com/awssupport/athenalab/mockdata/exercise1.py
@@ -22,7 +22,10 @@ class exercise1():
     def createBucket(self):
         print("bucketname:",self.s3bucket)
         print("region Name: ",boto3.session.Session().region_name)
-        self.s3.create_bucket(Bucket=self.s3bucket,CreateBucketConfiguration={
+        if(boto3.session.Session().region_name=='us-east-1'):
+            self.s3.create_bucket(Bucket=self.s3bucket)
+        else:
+            self.s3.create_bucket(Bucket=self.s3bucket,CreateBucketConfiguration={
     'LocationConstraint': boto3.session.Session().region_name})
 
     def json_serial(obj):
