953807414faff8f069150a3e9a12f31645e88bc9
sivakumar <srramas@amazon.com>
2018-11-22 13:12:12 +1100

    port change

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def cleandata():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=8080)
-    #app.run(debug=True, host='0.0.0.0', port=80)
+    #app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=80)
